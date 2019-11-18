bc8d407c33093df6d488e9afd8a3344982733ba9
Kyungrae <jeremiah.kim@hotmail.com>
2019-11-18 10:22:58 -0800

    Summary Comments completed

--- AsyncInn/Controllers/HomeController.cs
+++ AsyncInn/Controllers/HomeController.cs
@@ -8,7 +8,10 @@ namespace AsyncInn.Controllers
 {
     public class HomeController : Controller
     {
-
+        /// <summary>
+        /// Default HTTP GET route for / to display the entry point of AsyncInn app
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
             return View();
