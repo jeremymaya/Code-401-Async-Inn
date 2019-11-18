5608cc97bb773a252e849a412770f3ff6de9e172
Kyungrae <jeremiah.kim@hotmail.com>
2019-11-18 10:44:03 -0800

    data annotation for Amenities completed

--- AsyncInn/Models/Amenities.cs
+++ AsyncInn/Models/Amenities.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +10,8 @@ namespace AsyncInn.Models
     public class Amenities
     {
         public int ID { get; set; }
+        [Required]
+        [DisplayName("Amenity Name")]
         public string Name { get; set; }
 
         // Navigation Properties
