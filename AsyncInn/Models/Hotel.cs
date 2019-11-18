6eebeaa5d2153f75074a4940b3be3c5bef98f0e6
Kyungrae <jeremiah.kim@hotmail.com>
2019-11-18 11:09:52 -0800

    data annotation for Hotels completed

--- AsyncInn/Models/Hotel.cs
+++ AsyncInn/Models/Hotel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +10,18 @@ namespace AsyncInn.Models
     public class Hotel
     {
         public int ID { get; set; }
+        [Required]
+        [DisplayName("Hotel Name")]
         public string Name { get; set; }
+        [Required]
+        [DisplayName("Street Address")]
         public string StreetAddress { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
+        [RegularExpression(@"\d{3}-\d{3}-\d{4}")]
         public string Phone { get; set; }
 
         public ICollection<HotelRoom> HotelRoom { get; set; }
